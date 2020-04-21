7763794e786cd9ada2b0460e566faf1d8c755f9a
Will Swoveland <will.arctic@gmail.com>
2020-04-20 17:34:26 -0700

    this is such a mess

--- render.py
+++ render.py
@@ -12,116 +12,105 @@ import youtube_dl
 from PIL import Image, ImageOps
 import requests
 
-# Stuff for pyimgur
-CLIENT_ID = ""
-HEADERS = {"Authorization": "Client-ID {CLIENT_ID}"}
-IMGURUPLOADURL = "https://api.imgur.com/3/upload"
+with open('settings.json') as s
+    settings = json.load(s)
 
-# This information automatically goes into the description.
-BASEDESC = "If you\'re an owner of any song/picture on this channel \
-and want it removed, just message/email me and I\'ll do \
-my best to delete it as soon as possible.\n\nHave a nice \
-day! :)\n\nCopyright Disclaimer Under Section 107 of the \
-Copyright Act 1976, allowance is made for \"fair use\" \
-for purposes such as criticism, comment, news reporting, \
-teaching, scholarship, and research. Fair use is a use \
-permitted by copyright statute that might otherwise be \
-infringing. Non-profit, educational or personal use tips \
-the balance in favor of fair use."
+# Imgur authentication for image uploads
+CLIENT_ID = settings['ClientID']
+CLIENT_SECRET = settings['ClientSecret']
+headers = {"Authorization": "Client-ID " + CLIENT_ID}
+imgUploadEndpoint = "https://api.imgur.com/3/upload"
+
+# Assignments
+baseDesc = settings['basedesc']
 
 # Asks the user to supply a song URL or select an image file.
-SONGURLORFILE = easygui.buttonbox("Enter a URL or choose an audio file", choices=["File", "URL"])
-if SONGURLORFILE == "File":
-    SONGFILE = easygui.fileopenbox(msg=None, title=None, filetypes=["*.mp3"], multiple=False)
-    shutil.copy(SONGFILE, 'audio.mp3')
-elif SONGURLORFILE == "URL":
-    SONGURL = []
-    SONGURL.append(easygui.enterbox("Please enter a YouTube/SoundCloud URL:"))
+songSource = easygui.buttonbox("Enter a URL or choose an audio file", choices=["File", "URL"])
+if songSource == "File":
+    songFile = easygui.fileopenbox(msg=None, title=None, filetypes=["*.mp3"], multiple=False)
+    shutil.copy(songFile, 'audio.mp3')
+elif songSource == "URL":
+    songURL = [easygui.enterbox("Please enter a YouTube/SoundCloud URL:")]
 
 # Asks the user to supply an image URL or select an image file,
 # and uploads either to Imgur pre-jpg-conversion.
-IMAGEURLORFILE = easygui.buttonbox("Enter a URL or choose an image file", choices=["File", "URL"])
-if IMAGEURLORFILE == "File":
-    IMAGEFILE = easygui.fileopenbox(msg=None, title=None,\
+imgSource = easygui.buttonbox("Enter a URL or choose an image file", choices=["File", "URL"])
+if imgSource == "File":
+    imgFile = easygui.fileopenbox(msg=None, title=None,\
     filetypes=[["*.jpg", "*.png"]], multiple=False)
-    IMG = Image.open(IMAGEFILE)
-    RBG_IMG = IMG.convert('RGB')
-    RGB_IMG = IMG.save('image.png')
-    IMAGEUPLOAD = requests.post(
-        IMGURUPLOADURL,
-        headers=HEADERS,
-        DATA={
-            'image': b64encode(open('image.png', 'rb').read()),
+    img = Image.open(imgFile)
+    imgToJPG = img.convert('RGB')
+    imgToJPG = img.save('image.jpg')
+    uploadImg = requests.post(
+        imgUploadEndpoint,
+        headers=headers,
+        data={
+            'image': b64encode(open('image.jpg', 'rb').read()),
             'type': 'base64'
         }
     )
-    DATA = json.loads(IMAGEUPLOAD.text)['DATA']
-    IMAGELINK = DATA['link']
-    IMG = Image.open('image.png')
-    RGB_IMG = IMG.convert('RGB')
-    SIZE = (2560, 1440)
-    RESIZEDIMAGE = ImageOps.fit(RGB_IMG, SIZE, Image.ANTIALIAS)
-    RESIZEDIMAGE.save('image.jpg', format='JPEG', subsampling=0, quality=100)
-elif IMAGEURLORFILE == "URL":
-    IMAGEURL = easygui.enterbox("Please enter a direct link to an image.")
-    RESPONSE = requests.get(IMAGEURL)
-    IMG = Image.open(BytesIO(RESPONSE.content))
-    RGB_IMG = IMG.convert('RGB')
-    RGB_IMG.save('image.png')
-    IMAGEUPLOAD = requests.post(
-        IMGURUPLOADURL,
-        HEADERS=HEADERS,
-        DATA={
-            'image': b64encode(open('image.png', 'rb').read()),
+    data = json.loads(uploadImg.text)['data']
+    print(data)
+    imgurLink = data['link']
+    img = Image.open('image.jpg')
+    resizedImage = img.convert('RGB')
+    size = (2560, 1440)
+    resizedImage = ImageOps.fit(resizedImage, size, Image.ANTIALIAS)
+    resizedImage.save('image.jpg', format='JPEG', subsampling=0, quality=100)
+elif imageSource == "URL":
+    imageURL = easygui.enterbox("Please enter a direct link to an image.")
+    response = requests.get(imageURL)
+    img = Image.open(BytesIO(response.content))
+    imgToJPG = img.convert('RGB')
+    imgToJPG= img.save('image.jpg')
+    uploadImg = requests.post(
+        imgUploadEndpoint,
+        headers,
+        data={
+            'image': b64encode(open('image.jpg', 'rb').read()),
             'type': 'base64'
         }
     )
-    DATA = json.loads(IMAGEUPLOAD.text)['DATA']
-    IMAGELINK = DATA['link']
-    IMG = Image.open('image.png')
-    SIZE = (2560, 1440)
-    RESIZEDIMAGE = ImageOps.fit(RGB_IMG, SIZE, Image.ANTIALIAS)
-    RESIZEDIMAGE.save('image.jpg', format='JPEG', subsampling=0, quality=100)
+    data = json.loads(uploadImg.text)['data']
+    imgurLink = data['link']
+    img = Image.open('image.jpg')
+    size = (2560, 1440)
+    resizedImg = ImageOps.fit(imgToJpg, size, Image.ANTIALIAS)
+    resizedImg.save('image.jpg', format='JPEG', subsampling=0, quality=100)
 
 # Asks for song title, artist, artist links, any additions to
 # the description, and any additional tags.
-SONGTITLE = easygui.enterbox("Please enter a song title:")
-SONGARTIST = easygui.enterbox("Please enter an artist name:")
-ARTISTLINKS = []
-ARTISTLINKS = easygui.multenterbox("Please enter all of \
+title = easygui.enterbox("Please enter a song title:")
+artist = easygui.enterbox("Please enter an artist name:")
+artistLinks = []
+artistLinks = easygui.multenterbox("Please enter all of \
 the artist's social links.", "Social Links", ["Instagram",\
 "Twitter", "SoundCloud", "Spotify", "Bandcamp", "Website"])
-DESCRIPTIONADD = easygui.enterbox("Please enter any additions to \
+descAdds = easygui.enterbox("Please enter any additions to \
 the description you\'d like to add.")
-ADDEDTAGS = easygui.enterbox("Please enter a comma-seperated list of tags to add to the video.")
+tagAdds = "," + easygui.enterbox("Please enter a comma-seperated list of tags to add to the video.")
 
 # Some formatting for the description.
-while "" in ARTISTLINKS:
-    ARTISTLINKS.remove("")
-LISTARTISTSOCIALS = "\n".join(ARTISTLINKS)
-VIDEOTITLE = (SONGTITLE + " | " + SONGARTIST)
-LISTARTISTSOCIALS = "\n".join(ARTISTLINKS)
-VIDEOTAGS = "{Comma separated list, ending with a comma. These are your \
-    default tags for every video. If you don't use default tags, just \
-    set VIDEOTAGS to ADDEDTAGS}" + ADDEDTAGS
+while "" in artistLinks:
+    artistLinks.remove("")
+listArtistLinks = "\n".join(artistLinks)
+title = (title + " | " + artist)
+tags = settings['basetags'] + tagAdds
 
 # Generate thumbnail.jpg.
-shutil.copy('image.png', 'thumbnail.png')
-ORIGINAL_IMAGE = Image.open("thumbnail.png")
-ORIGINAL_IMAGE = ORIGINAL_IMAGE.convert('RGB')
-ORIGINAL_IMAGE.save('thumbnail.jpg', format='JPEG', subsampling=0, quality=100)
-ORIGINAL_IMAGE = Image.open("thumbnail.jpg")
-SIZE = (1920, 1080)
-RESIZEDIMAGED = ImageOps.fit(ORIGINAL_IMAGE, SIZE, Image.ANTIALIAS)
-RESIZEDIMAGED.save('thumbnail.jpg', format='JPEG', optimize=True, subsampling=0, quality=85)
+shutil.copy('image.jpg', 'thumbnail.jpg')
+thumbnail = Image.open("thumbnail.jpg")
+size = (1920, 1080)
+thumbnailGen = ImageOps.fit(thumbnail, size, Image.ANTIALIAS)
+thumbnailGen.save('thumbnail.jpg', format='JPEG', optimized=True,subsampling=0, quality=85)
 
 # Download a song from a valid source as defined by youtube-dl, and convert to audio.mp3.
 try:
-    SONGURL
+    songURL
 except NameError:
     pass
 else:
-    YTDL_OPTS = {
+    songDownload = {
         'format': 'bestaudio/best',
         'outtmpl': 'audio.%(ext)s',
         'postprocessors': [{
@@ -130,23 +119,23 @@ else:
             'preferredquality': '320',
             }],
         }
-    with youtube_dl.YoutubeDL(YTDL_OPTS) as ydl:
-        ydl.download(SONGURL)
+    with youtube_dl.YoutubeDL(songDownload) as ydl:
+        ydl.download(songURL)
 
 # Run nexrender. https://github.com/inlife/nexrender
 subprocess.call(['node', 'render.js'])
 
 # Upload the video using youtubeuploader. https://github.com/porjo/youtubeuploader
 subprocess.call(['youtubeuploader_windows_amd64.exe', '-filename', 'output.mp4', '-privacy', \
-    'public', '-thumbnail', 'thumbnail.jpg', '-tags', VIDEOTAGS, \
-    '-title', VIDEOTITLE, '-categoryId', '10', '-description', \
-    DESCRIPTIONADD + '\n\nArtist links:\n' + LISTARTISTSOCIALS + \
-    "\n\nImage link:\n" + IMAGELINK + "\n\n" + BASEDESC])
+    'public', '-thumbnail', 'thumbnail.jpg', '-tags', tags, \
+    '-title', title, '-categoryId', '10', '-description', \
+    descAdds + '\n\nArtist links:\n' + listArtistLinks + \
+    "\n\nImage link:\n" + imgurLink + "\n\n" + baseDesc])
 
 # Cleanup all downloaded, rendered and converted files.
-DIR_NAME = "./"
-WORKINGDIR = os.listdir(DIR_NAME)
-for item in WORKINGDIR:
+rootDir = "./"
+workingDir = os.listdir(rootDir)
+for item in workingDir:
     if item.endswith(".jpg") or item.endswith(".mp4") \
     or item.endswith(".mp3") or item.endswith(".png"):
-        os.remove(os.path.join(DIR_NAME, item))
+        os.remove(os.path.join(rootDir, item))
