a49670ca30df440970e00b94e881bf53ed843c7d
Will Swoveland <will.arctic@gmail.com>
2020-04-20 18:01:32 -0700

    ok

--- render.py
+++ render.py
@@ -12,130 +12,137 @@ import youtube_dl
 from PIL import Image, ImageOps
 import requests
 
-with open('settings.json') as s
-    settings = json.load(s)
+with open('settings.json') as config:
+    settings = json.load(config)
 
 # Imgur authentication for image uploads
-CLIENT_ID = settings['ClientID']
-CLIENT_SECRET = settings['ClientSecret']
-headers = {"Authorization": "Client-ID " + CLIENT_ID}
-imgUploadEndpoint = "https://api.imgur.com/3/upload"
+client_id = settings['ClientID']
+client_secret = settings['ClientSecret']
+headers = {"Authorization": "Client-ID " + client_id}
+api_url = "https://api.imgur.com/3/upload"
 
 # Assignments
-baseDesc = settings['basedesc']
+base_desc = settings['base_desc']
 
 # Asks the user to supply a song URL or select an image file.
-songSource = easygui.buttonbox("Enter a URL or choose an audio file", choices=["File", "URL"])
-if songSource == "File":
-    songFile = easygui.fileopenbox(msg=None, title=None, filetypes=["*.mp3"], multiple=False)
-    shutil.copy(songFile, 'audio.mp3')
-elif songSource == "URL":
-    songURL = [easygui.enterbox("Please enter a YouTube/SoundCloud URL:")]
+song_source = easygui.buttonbox(
+    "Enter a URL or choose an audio file", choices=["File", "URL"])
+if song_source == "File":
+    song_file = easygui.fileopenbox(
+        msg=None, title=None, filetypes=["*.mp3"], multiple=False)
+    shutil.copy(song_file, 'audio.mp3')
+elif song_source == "URL":
+    song_url = [easygui.enterbox("Please enter a YouTube/SoundCloud URL:")]
 
 # Asks the user to supply an image URL or select an image file,
 # and uploads either to Imgur pre-jpg-conversion.
-imgSource = easygui.buttonbox("Enter a URL or choose an image file", choices=["File", "URL"])
-if imgSource == "File":
-    imgFile = easygui.fileopenbox(msg=None, title=None,\
-    filetypes=[["*.jpg", "*.png"]], multiple=False)
-    img = Image.open(imgFile)
-    imgToJPG = img.convert('RGB')
-    imgToJPG = img.save('image.jpg')
-    uploadImg = requests.post(
-        imgUploadEndpoint,
+img_source = easygui.buttonbox(
+    "Enter a URL or choose an image file", choices=["File", "URL"])
+if img_source == "File":
+    img_file = easygui.fileopenbox(
+        msg=None, title=None, filetypes=[["*.jpg", "*.png"]], multiple=False)
+    img = Image.open(img_file)
+    img_to_jpg = img.convert('RGB')
+    img_to_jpg = img.save('image.jpg')
+    upload_img = requests.post(
+        api_url,
         headers=headers,
         data={
             'image': b64encode(open('image.jpg', 'rb').read()),
             'type': 'base64'
-        }
-    )
-    data = json.loads(uploadImg.text)['data']
+        })
+    data = json.loads(upload_img.text)['data']
     print(data)
-    imgurLink = data['link']
+    imgur_link = data['link']
     img = Image.open('image.jpg')
-    resizedImage = img.convert('RGB')
+    resized_image = img.convert('RGB')
     size = (2560, 1440)
-    resizedImage = ImageOps.fit(resizedImage, size, Image.ANTIALIAS)
-    resizedImage.save('image.jpg', format='JPEG', subsampling=0, quality=100)
-elif imageSource == "URL":
+    resized_image = ImageOps.fit(resized_image, size, Image.ANTIALIAS)
+    resized_image.save('image.jpg', format='JPEG', subsampling=0, quality=100)
+elif img_source == "URL":
     imageURL = easygui.enterbox("Please enter a direct link to an image.")
     response = requests.get(imageURL)
     img = Image.open(BytesIO(response.content))
-    imgToJPG = img.convert('RGB')
-    imgToJPG= img.save('image.jpg')
-    uploadImg = requests.post(
-        imgUploadEndpoint,
+    img_to_jpg = img.convert('RGB')
+    img_to_jpg = img.save('image.jpg')
+    upload_img = requests.post(
+        api_url,
         headers,
         data={
             'image': b64encode(open('image.jpg', 'rb').read()),
             'type': 'base64'
-        }
-    )
-    data = json.loads(uploadImg.text)['data']
-    imgurLink = data['link']
+        })
+    data = json.loads(upload_img.text)['data']
+    imgur_link = data['link']
     img = Image.open('image.jpg')
     size = (2560, 1440)
-    resizedImg = ImageOps.fit(imgToJpg, size, Image.ANTIALIAS)
-    resizedImg.save('image.jpg', format='JPEG', subsampling=0, quality=100)
+    resized_image = ImageOps.fit(img_to_jpg, size, Image.ANTIALIAS)
+    resized_image.save('image.jpg', format='JPEG', subsampling=0, quality=100)
 
 # Asks for song title, artist, artist links, any additions to
 # the description, and any additional tags.
 title = easygui.enterbox("Please enter a song title:")
 artist = easygui.enterbox("Please enter an artist name:")
-artistLinks = []
-artistLinks = easygui.multenterbox("Please enter all of \
-the artist's social links.", "Social Links", ["Instagram",\
-"Twitter", "SoundCloud", "Spotify", "Bandcamp", "Website"])
-descAdds = easygui.enterbox("Please enter any additions to \
+artist_links = []
+artist_links = easygui.multenterbox(
+    "Please enter all of \
+the artist's social links.", "Social Links",
+    ["Instagram", "Twitter", "SoundCloud", "Spotify", "Bandcamp", "Website"])
+desc_add = easygui.enterbox("Please enter any additions to \
 the description you\'d like to add.")
-tagAdds = "," + easygui.enterbox("Please enter a comma-seperated list of tags to add to the video.")
+tags_add = "," + easygui.enterbox(
+    "Please enter a comma-seperated list of tags to add to the video.")
 
 # Some formatting for the description.
-while "" in artistLinks:
-    artistLinks.remove("")
-listArtistLinks = "\n".join(artistLinks)
+while "" in artist_links:
+    artist_links.remove("")
+list_artist_links = "\n".join(artist_links)
 title = (title + " | " + artist)
-tags = settings['basetags'] + tagAdds
+tags = settings['basetags'] + tags_add
 
 # Generate thumbnail.jpg.
 shutil.copy('image.jpg', 'thumbnail.jpg')
 thumbnail = Image.open("thumbnail.jpg")
 size = (1920, 1080)
-thumbnailGen = ImageOps.fit(thumbnail, size, Image.ANTIALIAS)
-thumbnailGen.save('thumbnail.jpg', format='JPEG', optimized=True,subsampling=0, quality=85)
+thumbnail = ImageOps.fit(thumbnail, size, Image.ANTIALIAS)
+thumbnail.save(
+    'thumbnail.jpg', format='JPEG', optimized=True, subsampling=0, quality=85)
 
 # Download a song from a valid source as defined by youtube-dl, and convert to audio.mp3.
 try:
-    songURL
+    song_url
 except NameError:
     pass
 else:
-    songDownload = {
-        'format': 'bestaudio/best',
-        'outtmpl': 'audio.%(ext)s',
+    song_download = {
+        'format':
+        'bestaudio/best',
+        'outtmpl':
+        'audio.%(ext)s',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '320',
-            }],
-        }
-    with youtube_dl.YoutubeDL(songDownload) as ydl:
-        ydl.download(songURL)
+        }],
+    }
+    with youtube_dl.YoutubeDL(song_download) as ytdl:
+        ytdl.download(song_url)
 
 # Run nexrender. https://github.com/inlife/nexrender
 subprocess.call(['node', 'render.js'])
 
 # Upload the video using youtubeuploader. https://github.com/porjo/youtubeuploader
-subprocess.call(['youtubeuploader_windows_amd64.exe', '-filename', 'output.mp4', '-privacy', \
-    'public', '-thumbnail', 'thumbnail.jpg', '-tags', tags, \
-    '-title', title, '-categoryId', '10', '-description', \
-    descAdds + '\n\nArtist links:\n' + listArtistLinks + \
-    "\n\nImage link:\n" + imgurLink + "\n\n" + baseDesc])
+subprocess.call([
+    'youtubeuploader_windows_amd64.exe', '-filename', 'output.mp4', '-privacy',
+    'public', '-thumbnail', 'thumbnail.jpg', '-tags', tags, '-title', title,
+    '-categoryId', '10', '-description', desc_add + '\n\nArtist links:\n' +
+    list_artist_links + "\n\nImage link:\n" + imgur_link + "\n\n" + base_desc
+])
 
 # Cleanup all downloaded, rendered and converted files.
-rootDir = "./"
-workingDir = os.listdir(rootDir)
-for item in workingDir:
-    if item.endswith(".jpg") or item.endswith(".mp4") \
-    or item.endswith(".mp3") or item.endswith(".png"):
-        os.remove(os.path.join(rootDir, item))
+root_dir = "./"
+working_dir = os.listdir(root_dir)
+for item in working_dir:
+    if item.endswith(".jpg") or item.endswith(".mp4") or item.endswith(
+            ".mp3") or item.endswith(".png"):
+        os.remove(os.path.join(root_dir, item))
