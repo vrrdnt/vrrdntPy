6f2edcc74fb19de607f7c67e82efc21378a101b8
vrrdnt <will.arctic@gmail.com>
2020-06-04 13:23:01 -0700

    Some progress on video progression
    Need to rebase, lots of loose threads

--- modules/image_gen.py
+++ modules/image_gen.py
@@ -1,60 +1,43 @@
-# import shutil
-# from PIL import Image, ImageOps
-# import json
+import shutil
+from PIL import Image, ImageOps
+import json
+import requests
+from base64 import b64encode
+
+with open('../settings.json') as config:
+    settings = json.load(config)
+
+# Network open: Image.open(BytesIO(response.content))
+# Get link of imgur upload: data = json.loads(upload_img.text)['data']
+#                                  imgur_link = data['link']
+
+
+def fit_image(image):
+    img = Image.open(image)
+    image_dimensions = (settings['video_width'], settings['video_height'])
+    resized_image = ImageOps.fit(img, image_dimensions, Image.ANTIALIAS)
+    resized_image.save('../working/modified_image.jpg', format='JPEG', subsampling=0, quality=100)
+
+
+# # Upload image
+# # Imgur authentication section
+# client_id = settings['imgur_client_id']
 #
-# with open('../settings.json') as config:
-#     settings = json.load(config)
+# imgur_upload_endpoint = "https://api.imgur.com/3/upload"
 #
-# # TODO: This could go inside resources.py
+# headers = {"Authorization": "Client-ID " + client_id}
 #
-# # Image formatting
-# # Asks the user to supply an image URL or select an image file,
-# # and uploads either to Imgur pre-jpg-conversion.
-# img_source = easygui.buttonbox(
-#     "Enter a URL or choose an image file", choices=["File", "URL"])
-# if img_source == "File":
-#     img_file = easygui.fileopenbox(
-#         msg=None, title=None, filetypes=[["*.jpg", "*.png"]], multiple=False)
-#     img = Image.open(img_file)
-#     img_to_jpg = img.convert('RGB')
-#     img_to_jpg = img.save('image.jpg')
-#     upload_img = requests.post(
-#         api_url,
+#
+# def imgur_upload(image): # TODO: This is the file upload one. Need to mix URL/disk image upload.
+#     image_upload = requests.post(
+#         imgur_upload_endpoint,
 #         headers=headers,
 #         data={
 #             'image': b64encode(open('image.jpg', 'rb').read()),
 #             'type': 'base64'
 #         })
-#     data = json.loads(upload_img.text)['data']
-#     print(data)
+#     data = json.loads(imgur_upload.text)['data']
 #     imgur_link = data['link']
-#     img = Image.open('image.jpg')
-#     resized_image = img.convert('RGB')
-#     size = (2560, 1440)
-#     resized_image = ImageOps.fit(resized_image, size, Image.ANTIALIAS)
-#     resized_image.save('image.jpg', format='JPEG', subsampling=0, quality=100)
-# elif img_source == "URL":
-#     imageURL = easygui.enterbox("Please enter a direct link to an image.")
-#     response = requests.get(imageURL)
-#     img = Image.open(BytesIO(response.content))
-#     img_to_jpg = img.convert('RGB')
-#     img_to_jpg = img.save('image.jpg')
-#     upload_img = requests.post(
-#         api_url,
-#         headers,
-#         data={
-#             'image': b64encode(open('image.jpg', 'rb').read()),
-#             'type': 'base64'
-#         })
-#     data = json.loads(upload_img.text)['data']
-#     imgur_link = data['link']
-#     img = Image.open('image.jpg')
-#     size = (2560, 1440)
-#     resized_image = ImageOps.fit(img_to_jpg, size, Image.ANTIALIAS)
-#     resized_image.save('image.jpg', format='JPEG', subsampling=0, quality=100)
-#
-#
-#
 #
 #
 #
@@ -65,7 +48,7 @@
 #
 # shutil.copy('image.jpg', 'thumbnail.jpg')
 # thumbnail = Image.open("thumbnail.jpg")
-# size = (1920, 1080)
+# size = (settings['thumbnail_width'], settings['thumbnail_height'])
 # thumbnail = ImageOps.fit(thumbnail, size, Image.ANTIALIAS)
 # thumbnail.save(
 #     'thumbnail.jpg', format='JPEG', optimized=True, subsampling=0, quality=85)
--- modules/video.py
+++ modules/video.py
@@ -1,20 +1,28 @@
 import moviepy.editor as mpy
-from math import pi
+from mutagen.mp3 import MP3
+from image_gen import fit_image
+import json
 
-test_image_source = "./image.jpg"
-test_audio_source = "./changes.mp3"
+with open('../settings.json') as config:
+    settings = json.load(config)
 
-test_image = mpy.ImageClip(test_image_source).set_position('center', 'center')
+test_image_source = "../working/image.jpg"
+test_audio_source = "../working/audio.mp3"
+
+fit_image(test_image_source)
+final_image = "../working/modified_image.jpg"
+
+mutagen_load = MP3(test_audio_source)
+
+test_image = mpy.ImageClip(final_image).set_position('center', 'center')
 test_audio = mpy.AudioFileClip(test_audio_source)
 
-video_size = (1920, 1080)
+video_size = (settings['video_width'], settings['video_height'])
 
-
-# TODO: use PIL to fit image to dimensions set in settings. mpy can't do it.
 video = mpy.CompositeVideoClip(
     [
         test_image
     ],
-    size=video_size).set_duration(1).set_audio(test_audio)
+    size=video_size).set_duration(mutagen_load.info.length).set_audio(test_audio)
 
-video.write_videofile('video_with_python.mp4', fps=24)
+video.write_videofile('../working/video.mp4', fps=24)
